23d25451348d33adedac61eff886647912a1a3bf
abantakish <Aban.Taki@yh.nackademin.se>
2024-11-12 22:34:57 +0100

    smile

--- src/components/firstPage/firstPage.jsx
+++ src/components/firstPage/firstPage.jsx
@@ -11,46 +11,51 @@ import "./switcher.css";
 const FirstPage = () => {
   return (
     <section id="firstPageWrapper" className="first-page">
-      <header>
-        <section className="headerSideSection headerLeftSection">
-          <img src={solidIcon} alt="Features" />
-        </section>
-        <section className="headerSideSection headerRightSection">
-          <p>Dark Mode</p>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>{" "}
-            {/* Make sure this class matches the CSS */}
-          </label>
-          <p id="signInUp">
-            <i className="fa-solid fa-user"></i> Sign in / up
-          </p>
-        </section>
-      </header>
+      <section id="firstPage">
+        <header>
+          <section className="headerSideSection headerLeftSection">
+            <img src={solidIcon} alt="Features" />
+            <p>Features</p>
+          </section>
+          <section className="headerSideSection headerRightSection">
+            <p>Dark Mode</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
+            <p id="signInUp">
+              <i className="fa-thin fa-user-large"></i> Sign in / up
+            </p>
+          </section>
+        </header>
 
-      <section id="firstPageContent">
-        <section id="downloadSection">
-          <div id="headers">
-            <h1>Manage All Your</h1>
-            <h3>Money in One App</h3>
-          </div>
-          <div id="paragraphs">
-            <p>We offer you a new generation of mobile banking.</p>
-            <p>Save, spend & manage money in your pocket.</p>
-          </div>
-          <section id="downloadButtons">
-            <img src={appStoreIcon} alt="Download on App Store" />
-            <img src={googlePlayIcon} alt="Get it on Google Play" />
+        <section id="firstPageContent">
+          <section id="downloadSection">
+            <div id="headers">
+              <h1 className="header-text">Manage All Your</h1>
+              <h1 className="header-text">Money in One App</h1>
+            </div>
+            <div id="paragraphs">
+              <p>We offer you a new generation of mobile banking.</p>
+              <p>Save, spend & manage money in your pocket.</p>
+            </div>
+            <div id="downloadButtons">
+              <img src={appStoreIcon} alt="Download on App Store" />
+              <img src={googlePlayIcon} alt="Get it on Google Play" />
+            </div>
+            <div id="discoverSection">
+              <button className="discover-more-btn">
+                <i className="fa fa-chevron-down"></i>
+              </button>
+              <span>Discover More</span>
+            </div>
+          </section>
+
+          <section id="twoPhoneImages">
+            <img src={doublePhonesIcon} alt="Two Mobile Phones" />
           </section>
         </section>
-
-        <section id="discoverSection">
-          <img src={doublePhonesIcon} alt="Discover More" />
-          <span>Discover More</span>
-        </section>
       </section>
-
-      <div className="divider"></div>
     </section>
   );
 };
